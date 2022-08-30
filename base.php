f996e16fa324d6a60f8747b2f4eb580c34ef93a3
Kaue <kaue1castelani@gmail.com>
2022-08-29 21:34:06 -0300

    navegação php

---
+++ base.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SENAGRAN</title>
+
+    <!-- CSS -->
+    <?php include('inc/css.php');
+    ?>
+    <!-- JS -->
+    <?php include_once('inc/js.php');
+    ?>
+
+
+</head>
+
+<body>
+    <div class="container">
+        <!-- MENU -->
+        <?php 
+            include('inc/menu.php');
+            include('inc/menu.php');
+            include('inc/menu.php');
+        ?>
+        <!-- /MENU -->
+
+        <!-- CONTEUDO -->
+        <div>
+
+        </div>
+        <!-- /CONTEUDO -->
+
+        <!-- RODAPE -->
+
+        <!-- /RODAPE -->
+
+    </div>
+</body>
+
+</html>
---
+++ inc/menu.php
@@ -0,0 +1,18 @@
+<div>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">SenaGran</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="navbar-nav">
+                    <a class="nav-link" href="home.php">Home</a>
+                    <a class="nav-link" href="postagem.php">Postagens</a>
+                    <a class="nav-link" href="usuarios.php">Usuarios</a>
+                    <a class="nav-link" href="sair.php">Sair</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</div>
